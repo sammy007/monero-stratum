25d60b8f72548c4b505457bf1fec81c57ceb4eb4
sammy007 <sammy007@users.noreply.github.com>
2018-05-25 12:46:29 +0200

    Add support for Aeon HF

--- hashing/src/hashing.c
+++ hashing/src/hashing.c
@@ -1,7 +1,8 @@
 #include "crypto/hash-ops.h"
 
 void cryptonight_hash(const char* input, char* output, uint32_t len) {
-    cn_slow_hash(input, len, output, 1);
+    const int variant = input[0] >= 7 ? input[0] - 6 : 0;
+    cn_slow_hash(input, len, output, 1, variant, 0);
 }
 
 void cryptonight_fast_hash(const char* input, char* output, uint32_t len) {
