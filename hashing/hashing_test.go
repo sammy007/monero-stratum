b008ad691380873793e8b9a5d8f619bb23f4acd8
Dagon Harett <dagon@fractra.com>
2019-02-28 01:05:03 +0000

    CryptonightR compatibility
    Now compatible with Monero v0.14.0.0 which requires the block height
    as argument in cn_slow_hash. This is mandatory for the hardfork to block
    major_version 10 which brings a new PoW algorithm (CryptonightR).
    
    Block height is now an argument in all calls to the slow hash.
    
    Hashing tests updated to use a block blob with major_version 10.
    
    README now mentioning v0.14.0.0 as minimum Monero version.
    
    circle-ci updated for v0.14.0.0

--- hashing/hashing_test.go
+++ hashing/hashing_test.go
@@ -5,12 +5,12 @@ import "log"
 import "encoding/hex"
 
 func TestHash(t *testing.T) {
-	blob, _ := hex.DecodeString("01009091e4aa05ff5fe4801727ed0c1b8b339e1a0054d75568fec6ba9c4346e88b10d59edbf6858b2b00008a63b2865b65b84d28bb31feb057b16a21e2eda4bf6cc6377e3310af04debe4a01")
-	hashBytes := Hash(blob, false)
+	blob, _ := hex.DecodeString("a1009091e4aa05ff5fe4801727ed0c1b8b339e1a0054d75568fec6ba9c4346e88b10d59edbf6858b2b00008a63b2865b65b84d28bb31feb057b16a21e2eda4bf6cc6377e3310af04debe4a01")
+	hashBytes := Hash(blob, false, 0)
 	hash := hex.EncodeToString(hashBytes)
 	log.Println(hash)
 
-	expectedHash := "a70a96f64a266f0f59e4f67c4a92f24fe8237c1349f377fd2720c9e1f2970400"
+	expectedHash := "9d38ea4c527970fee1c4f89272a6fb787de71fc95bba7592eaed6e20b7f7478e"
 
 	if hash != expectedHash {
 		t.Error("Invalid hash")
@@ -19,7 +19,7 @@ func TestHash(t *testing.T) {
 
 func TestHash_fast(t *testing.T) {
 	blob, _ := hex.DecodeString("01009091e4aa05ff5fe4801727ed0c1b8b339e1a0054d75568fec6ba9c4346e88b10d59edbf6858b2b00008a63b2865b65b84d28bb31feb057b16a21e2eda4bf6cc6377e3310af04debe4a01")
-	hashBytes := Hash(blob, true)
+	hashBytes := Hash(blob, true, 0)
 	hash := hex.EncodeToString(hashBytes)
 	log.Println(hash)
 
@@ -44,21 +44,21 @@ func TestFastHash(t *testing.T) {
 }
 
 func BenchmarkHash(b *testing.B) {
-	blob, _ := hex.DecodeString("0100b69bb3aa050a3106491f858f8646d3a8d13fd9924403bf07af95e6e7cc9e4ad105d76da27241565555866b1baa9db8f027cf57cd45d6835c11287b210d9ddb407deda565f8112e19e501")
+	blob, _ := hex.DecodeString("a100b69bb3aa050a3106491f858f8646d3a8d13fd9924403bf07af95e6e7cc9e4ad105d76da27241565555866b1baa9db8f027cf57cd45d6835c11287b210d9ddb407deda565f8112e19e501")
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		Hash(blob, false)
+		Hash(blob, false, 0)
 	}
 }
 
 func BenchmarkHashParallel(b *testing.B) {
-	blob, _ := hex.DecodeString("0100b69bb3aa050a3106491f858f8646d3a8d13fd9924403bf07af95e6e7cc9e4ad105d76da27241565555866b1baa9db8f027cf57cd45d6835c11287b210d9ddb407deda565f8112e19e501")
+	blob, _ := hex.DecodeString("a100b69bb3aa050a3106491f858f8646d3a8d13fd9924403bf07af95e6e7cc9e4ad105d76da27241565555866b1baa9db8f027cf57cd45d6835c11287b210d9ddb407deda565f8112e19e501")
 	b.ResetTimer()
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			Hash(blob, false)
+			Hash(blob, false, 0)
 		}
 	})
 }
@@ -68,7 +68,7 @@ func BenchmarkHash_fast(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		Hash(blob, true)
+		Hash(blob, true, 0)
 	}
 }
 
