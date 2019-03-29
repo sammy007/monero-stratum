80c7dd068cba7f3388a433ebb1d95721829e490e
stoffu <stoffu@protonmail.ch>
2019-03-29 18:55:15 +0900

    Aeon v8: PoW switch to KangarooTwelve

--- hashing/src/hashing.c
+++ hashing/src/hashing.c
@@ -1,8 +1,15 @@
 #include "crypto/hash-ops.h"
 
 void cryptonight_hash(const char* input, char* output, uint32_t len) {
-    const int variant = input[0] >= 7 ? input[0] - 6 : 0;
-    cn_slow_hash(input, len, output, 1, variant, 0);
+    if (input[0] >= 8)
+    {
+        k12(input, len, output);
+    }
+    else
+    {
+        const int variant = input[0] >= 7 ? input[0] - 6 : 0;
+        cn_slow_hash(input, len, output, 1, variant, 0);
+    }
 }
 
 void cryptonight_fast_hash(const char* input, char* output, uint32_t len) {
